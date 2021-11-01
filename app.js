0909160d2adfa379dfe73e835f0774b209b2674a
Jaygeekacademy <javier.castano003@mymdc.net>
2021-11-01 22:38:34 +0000

    domain g

---
+++ app.js
@@ -0,0 +1,17 @@
+  let pronoun = ['the','our', 'my'];
+  let adj = ['great', 'big', 'small' ];
+  let noun = ['jogger','racoon', 'turtle'];
+
+for(let p of pronoun) {
+
+    for (let a of adj) {
+     
+     for(let n of noun) {
+        console.log (p+a+n+'.com')
+
+     }
+       
+    } 
+
+}
+
